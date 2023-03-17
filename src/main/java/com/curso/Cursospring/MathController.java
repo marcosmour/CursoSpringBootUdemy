e3cbc7a07d1e0993ec340dc234349692a3c422b4
Marcos Moura <marcosmoura201@hotmail.com>
2023-03-17 20:24:29 -0300

    Criando a logica do exercicio

--- src/main/java/com/curso/Cursospring/MathController.java
+++ src/main/java/com/curso/Cursospring/MathController.java
@@ -13,8 +13,22 @@ public class MathController {
 	private final AtomicLong counter = new AtomicLong();
 	
 	@RequestMapping(value = "/sum/{numberOne}/{numberTwo}", method = RequestMethod.GET)
-	public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) {
-		return 1D;
+	public Double sum(@PathVariable(value = "numberOne") String numberOne, @PathVariable(value = "numberTwo") String numberTwo) throws Exception {
+		
+		if(!isNumeric(numberOne) || !isNumeric(numberTwo)) {
+			throw new Exception();
+		}
+		
+		return convertToDouble(numberOne) + convertToDouble(numberTwo);
+	}
+	
+	private Double convertToDouble(String strNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	private boolean isNumeric(String strNumber) {
+		return false;
 	}
 	
 
